22922afb73c7f575bfbbfec5f2ab865e4a1dcd49
bautista-gilardon <bautistagilardon@gmail.com>
2023-05-30 12:43:51 -0300

    FilasParecidas2

--- filasParecidas.py
+++ filasParecidas.py
@@ -9,48 +9,40 @@ def filasParecidas(matriz: List[List[int]]) -> bool :
 
   if len(matriz) > 1 and type(matriz[0]) == list:
     n : int = matriz[1][0] - matriz[0][0]
-    print(f'N = {n}')
     for i in range(1, len(matriz)):
       for j in range(len(matriz[0])):
-        print(matriz[i][j], matriz[i-1][j], (matriz[i-1][j] + n))
         if matriz[i][j] != (matriz[i-1][j] + n):
           res = False
-        print(res)
-  else:
-    print(matriz, len(matriz))
-  
-  print(res)
 
   return res
 
 
 
-def test():
-  m1 = [1]
-  m2 = [1,2,3]
-  m2ymedio = [[1,2,3],[4,5,6]]
-  m3 = [[1],[2],[3]]
-  m4 = [[1,2,3],[4,5,6],[7,8,9]]
-  m5 = [[1,2,3],[4,5,6],[7,9,9]]
-  matrices = [m1,m2,m2ymedio,m3,m4,m5]
-  for m in matrices:
-    print(m)
-    filasParecidas(m)
-    print()
-
-test()
+# def test():
+#   m1 = [1]
+#   m2 = [1,2,3]
+#   m2ymedio = [[1,2,3],[4,5,6]]
+#   m3 = [[1],[2],[4]]
+#   m4 = [[1,2,3],[4,5,6],[7,8,9]]
+#   m5 = [[1,2,3],[4,5,6],[7,9,9]]
+#   matrices = [m1,m2,m2ymedio,m3,m4,m5]
+#   for m in matrices:
+#     print(m)
+#     print(filasParecidas(m))
+#     print()
 
 
-# if __name__ == '__main__':
-#   filas = int(input())
-#   columnas = int(input())
+
+if __name__ == '__main__':
+  filas = int(input())
+  columnas = int(input())
  
-#   matriz = []
+  matriz = []
  
-#   for i in range(filas):         
-#     fila = input()
-#     if len(fila.split()) != columnas:
-#       print("Fila " + str(i) + " no contiene la cantidad adecuada de columnas")
-#     matriz.append([int(j) for j in fila.split()])
+  for i in range(filas):         
+    fila = input()
+    if len(fila.split()) != columnas:
+      print("Fila " + str(i) + " no contiene la cantidad adecuada de columnas")
+    matriz.append([int(j) for j in fila.split()])
   
-#   print(filasParecidas(matriz))
+  print(filasParecidas(matriz))
